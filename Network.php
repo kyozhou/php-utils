c16134da8545b913691bf589f5e5a9fa44945d11
周斌 <kyozhou@sina.com>
2016-12-07 11:45:31 +0800

    fix name bug

--- Network.php
+++ Network.php
@@ -6,7 +6,7 @@ class Network {
     static function getEthIp($interface = 'eth0') {
         $interface = escapeshellarg($interface);
         $pattern = "/inet addr:(\d+\.\d+\.\d+\.\d+)/";
-        $text = shell_exec("ifconfig $internface");
+        $text = shell_exec("ifconfig $interface");
         preg_match($pattern, $text, $matches);
         return empty($matches[1]) ? '' : $matches[1];
     }
